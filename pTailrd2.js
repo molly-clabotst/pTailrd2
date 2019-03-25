9e23162303a2a32deee77cbb9a0f8a16da0b2626
Molly <qc8659ba@go.minneapolis.edu>
2019-03-25 11:48:00 -0500

    latest update 3/25

--- pTailrd2.js
+++ pTailrd2.js
@@ -1,13 +1,16 @@
-window.onscroll = function () {
-    {myFunction()}
-};
-var navbar = document.getElementById(navbar);
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var side = document.getElementById("mySidenav");
 var sticky = navbar.offsetTop;
+var bStick = side.offsetTop;
 
 function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    }else {
-        navbar.classList.remove("sticky")
+    if (window.pageYOffset >= sticky&& window.pageYOffset>=bStick) {
+        navbar.classList.add("sticky");
+        // side.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+        // side.classList.remove("sticky")
     }
 }
